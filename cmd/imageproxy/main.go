825ba5aa69e43f8f7c893bf117209ac9bcc454cd
Bryan Jackson <bryan@code.us>
2017-03-01 22:57:41 -0600

    change image server to return cloudfront url

--- cmd/imageproxy/main.go
+++ cmd/imageproxy/main.go
@@ -24,11 +24,11 @@ import (
 	"net/url"
 	"strings"
 
+	"github.com/code-us/imageproxy"
 	"github.com/gregjones/httpcache"
 	"github.com/gregjones/httpcache/diskcache"
 	"github.com/peterbourgon/diskv"
 	"sourcegraph.com/sourcegraph/s3cache"
-	"willnorris.com/go/imageproxy"
 )
 
 // goxc values
